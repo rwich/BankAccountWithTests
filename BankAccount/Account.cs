2cae33c7c3deacc2deaa6250adf365762578f078
rwich <rwich@users.noreply.github.com>
2017-08-01 14:18:52 -0700

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a bank account
+    /// </summary>
+    public class Account
+    {
+        public Account()
+        {
+
+        }
+
+        public Account(double initialBalance)
+        {
+            if(initialBalance < 0)
+            {
+                throw new ArgumentException();
+            }
+            Balance = initialBalance;
+        }
+
+        public double Balance
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// Adds an amount to the balance
+        /// </summary>
+        /// <param name="amt">Amount to be added</param>
+        public void Deposit(double amt)
+        {
+            if (amt > 0)
+            {
+                Balance += amt;
+            }
+            else
+            {
+                throw new ArgumentException("Deposit must be greater than 0!");
+            }
+        }
+
+        /// <summary>
+        /// Subtacts an amount from the balance
+        /// </summary>
+        /// <param name="amt">Amount to be subtracted</param>
+        public void Withdraw(double amt)
+        {
+            if(amt < 0)
+            {
+                throw new ArgumentException("Withdraw amount cannot be negative");
+            }
+            Balance -= amt;
+        }
+    }
+}
---
+++ BankAccountTests/AccountTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        Account acc;
+
+        [TestInitialize]
+        public void InitTests()
+        {
+            acc = new Account();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateAccountWithNegativeBalanceThrowEx()
+        {
+            double initialBalance = -1;
+            Account acc = new Account(initialBalance);
+        }
+
+        [TestMethod]
+        public void CreateAccountWithPositiveInitialBalance()
+        {
+            double initialBalance = 100;
+            Account acc = new Account(initialBalance);
+            Assert.AreEqual(initialBalance, acc.Balance);
+        }
+
+        [TestMethod()]
+        [TestCategory("DepositTests")]
+        public void DepositPositiveAmount()
+        {
+            //Arrange
+            Account acc = new Account();
+            double initialBalance = 0;
+            double depositAmount = 10;
+            double expectedBalance = initialBalance + depositAmount;
+
+            //Act
+            acc.Deposit(depositAmount);
+
+            //Assert
+            Assert.AreEqual(expectedBalance, acc.Balance);
+        }
+
+        [TestMethod]
+        [TestCategory("DepositTests")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DepositNegativeAmountShouldThrowException()
+        {
+            //Arrange
+            Account a = new Account();
+            //double initialBalance = 0;
+            double depositAmount = -5;
+
+
+            //Act
+            a.Deposit(depositAmount);
+
+            //Assert
+            //Assert is handled by ExpectedException attribute
+        }
+
+        [TestMethod]
+        [TestCategory("WithdrawTests")]
+        public void WithdrawPositiveAmount()
+        {
+            Account acc = new Account();
+            //Arrange
+            double initialBalance = 0;
+            double withdrawAmount = 10;
+            double expectedBalance = initialBalance - withdrawAmount;
+
+            //Act
+            acc.Withdraw(withdrawAmount);
+
+            //Assert
+            Assert.AreEqual(expectedBalance, acc.Balance);
+        }
+
+        [TestMethod]
+        [Priority(1)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WithdrawNegativeAmountShouldThrowException()
+        {
+            Account acc = new Account();
+            double withdrawAmount = -10;
+            acc.Withdraw(withdrawAmount);
+        }
+    }
+}
